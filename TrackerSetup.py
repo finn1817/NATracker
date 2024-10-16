220f989f3910bb462cdc3fc78edfe6e8634b8378
Miles Calloway <mcall@mcallbos.co>
2024-10-15 21:24:10 -0400

    Required Argument

--- TrackerSetup.py
+++ TrackerSetup.py
@@ -2,7 +2,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Simple Journal Tracker Setup')
-    parser.add_argument('--dir', type=str, help='Directory to track')
+    parser.add_argument('--dir', type=str, help='Directory to track', required=True)
     parser.add_argument('--remove', type=bool, help='Remove previous tracking')
 
     args = parser.parse_args()
