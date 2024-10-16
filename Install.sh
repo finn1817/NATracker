1db29db2bbee6e5c785afbdc55cf85f9c2c0865b
Miles Calloway <mcall@mcallbos.co>
2024-10-15 21:47:49 -0400

    Test

--- Install.sh
+++ Install.sh
@@ -14,6 +14,15 @@ if [ -L "/usr/local/bin/NATracker" ]; then
     rm /usr/local/bin/NATracker
 fi
 
+#check for incron
+if [ ! -x "$(command -v incrontab)" ]; then
+    echo "Installing incron"
+    apt update
+    apt install -y incron
+    dnf install -y incron
+    yum install -y incron
+fi
+
 
 git clone "$REPO_URL" /opt/NATracker
 
--- TrackerSetup.py
+++ TrackerSetup.py
@@ -1,13 +1,54 @@
 import argparse
+import os
 
 def main():
     parser = argparse.ArgumentParser(description='Simple Journal Tracker Setup')
     parser.add_argument('--dir', type=str, help='Directory to track', required=True)
-    parser.add_argument('--remove', type=bool, help='Remove previous tracking')
+    #parser.add_argument('--remove', type=bool, help='Remove previous tracking') impliment later
+    #parser.add_argument('--update', type=bool, help='Update NATracker (requires root)') skipping this for now
+
+
 
     args = parser.parse_args()
-    
+    #if args.update:
 
+        
+
+def addTracking(directory):
+    #check that the directory exists
+    if not os.path.exists(directory):
+        print(f"Directory {directory} does not exist.")
+        return
+    #interfacing with incron
+    #root dir will be /opt/NAJournal due to install
+
+    #create a new file in /etc/incron.d/NAJournal
+    commandForFileAdd = directory + " IN_MODIFY python3 /opt/NAJournal/Events/changeFile.py $@/$#"
+    commandForFileRemove = directory + " IN_DELETE python3 /opt/NAJournal/Events/delFile.py $@/$#"
+    commandForFileAdd = directory + " IN_CREATE python3 /opt/NAJournal/Events/addFile.py $@/$#"
+    
+    #write to dir in .incron.d file
+    #check if the file exists
+    if not os.path.exists("/etc/incron.d/NAJournal"):
+        with open("/etc/incron.d/NAJournal", "w") as file:
+            file.write("#Start " + directory + "\n")
+            file.write(commandForFileAdd + "\n")
+            file.write(commandForFileRemove + "\n")
+            file.write(commandForFileAdd + "\n")
+    else:
+        #check if the directory is already being tracked
+        with open("/etc/incron.d/NAJournal", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                if line == "#Start " + directory + "\n":
+                    print(f"{directory} is already being tracked.")
+                    return
+        with open("/etc/incron.d/NAJournal", "a") as file:
+            file.write("#Start " + directory + "\n")
+            file.write(commandForFileAdd + "\n")
+            file.write(commandForFileRemove + "\n")
+            file.write(commandForFileAdd + "\n")    
+    
     
 
 if __name__ == "__main__":
