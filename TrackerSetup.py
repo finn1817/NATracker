781e21eca0d46a84da588ec308fde7b03df61a9f
Miles Calloway <mcall@mcallbos.co>
2024-10-16 02:12:38 -0400

    fix

--- TrackerSetup.py
+++ TrackerSetup.py
@@ -19,7 +19,7 @@ def main():
     parser.add_argument('--dir', type=str, help='Directory to track')
     parser.add_argument('--remove', action='store_true', help='Remove previous tracking')
     parser.add_argument('--list', action='store_true', help='List all tracked directories')
-    parser.add_argument('--format', type='store_true', help='Reset Everything')
+    parser.add_argument('--format', action='store_true', help='Reset Everything')
     #parser.add_argument('--update', type=bool, help='Update NATracker (requires root)') skipping this for now
 
 
@@ -30,11 +30,9 @@ def main():
             removeTracking(args.dir)
         else:
             addTracking(args.dir)
-    elif args.l:
-        listTracking()
-    else:
-        print("No arguments provided. Use --help for help.")
     
+    if args.list:
+        listTracking()
 
 
         
