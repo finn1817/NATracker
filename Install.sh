78cf4049e94124d9bb01bee2686b5042d89d095c
Zach Stofko <90708909+PresidentZach@users.noreply.github.com>
2024-11-07 11:54:28 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -25,7 +25,7 @@ sudo pip install inotify_simple
 sudo apt install git
 
 #autoload ThingThatWillRunOnStartup.py
-Startup_path="$(pwd)/ThingThatWillRunOnStartup.py"
+Startup_path="$/opt/NATracker/ThingThatWillRunOnStartup.py"
 chmod +x "$Startup_path" #making sure the script is executable
 #use crontab to schedule the python script to run on startup (@reboot)
 (crontab -l; echo "@reboot python3 \"$Startup_path\"") | crontab -
