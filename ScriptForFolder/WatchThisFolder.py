b8f0475ab25876ffb0ceb40b1832e6a5fa50d1c1
Miles Calloway <mcall@mcallbos.co>
2024-11-19 22:27:49 -0500

    sdafdsafds

--- ScriptForFolder/WatchThisFolder.py
+++ ScriptForFolder/WatchThisFolder.py
@@ -41,8 +41,8 @@ def watcher():
     while True:
         time.sleep(1)
         #check if this script still exsists
-        if not os.path.exists(os.path.realpath(__file__)):
-            exit()
+        #if not os.path.exists(os.path.realpath(__file__)):
+        #    exit()
 
         #check for any consecutive file creations and deletions
         creation = []
