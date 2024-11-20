792e8964c453ac2438e7586ce574f006df0ee41f
Miles Calloway <mcall@mcallbos.co>
2024-11-19 21:08:40 -0500

    Struggle

--- ConfigStuff/Watchers.py
+++ ConfigStuff/Watchers.py
@@ -71,8 +71,7 @@ def addWatcher(location):
         return
     #as subprocess
     #subprocess.Popen(["python3", location + "/.NATracker/WatchThisFolder.py"],stdin=None, stdout=None, stderr=None, shell=True)
-    os.spawnl(os.P_NOWAIT, "python3", "python3", location + "/.NATracker/WatchThisFolder.py")
-    
+    subprocess.Popen(["python3", location + "/.NATracker/WatchThisFolder.py"], stdin=None, stdout=None, stderr=None, close_fds=True)
 def removeWatcher(location):
     exsistingWatcher = checkForWatcher(location)
     if (exsistingWatcher == False):
