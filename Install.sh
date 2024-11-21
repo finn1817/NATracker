2f30fbd4272a6f59bf49c8415af6a5046cb3f414
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2024-11-21 01:33:23 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -63,7 +63,7 @@ if [ $? -eq 0 ]; then
 	Version=0.0
 	Type=Application
 	Name=NATracker
-	Exec=/opt/NATracker/Runners/FolderTrackerGUI -desktop
+	Exec=bash FolderTrackerGUI -desktop
 	Icon=/opt/NATracker/GUI/icon2.png
 	Terminal=false
 	Categories=Utility;Application;" > "$ICON"
