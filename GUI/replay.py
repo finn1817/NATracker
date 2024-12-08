47ba10cc099e034fdb48a187bda3b0669cebd4a8
Zach Stofko <90708909+PresidentZach@users.noreply.github.com>
2024-12-08 02:01:11 -0500

    update comments

--- GUI/replay.py
+++ GUI/replay.py
@@ -1,10 +1,10 @@
 from gi.repository import Gtk
 import os
-import pickle
+import pickle # for stuff Miles worked on
 from dataclasses import dataclass
-import datetime
+import datetime # for handling timestamps in journal entries
 
-
+# representing a journal containing entries about file changes
 @dataclass
 class Journal:
     JournalID = 0
@@ -18,68 +18,72 @@ class Journal:
 
 # ----------------------------------------------------------------------------------------------
 
-#tab settings
+# GUI settings
 BORDER_WIDTH = 12
 MAX_REPLAY_ENTRIES = 50
 
-#list of the journal entries as strings
+# list of the journal entries as strings
 replay_list = []
-global journal #global variable for the currently selected journal
+global journal #g lobal variable for the currently selected journal
 
-#get python file location
+# get python file location
 pythonFileLocation = os.path.realpath(__file__)
-#remove the file name
+# remove the file name
 pythonFileLocation = pythonFileLocation.rsplit("/", 1)[0]
-#remove the last folder
+# remove the last folder
 pythonFileLocation = pythonFileLocation.rsplit("/", 1)[0]
 currentDir = pythonFileLocation
 
+# lists to hold data and timestamps for journal entries
+# used for listing in the listbox
 stringList = []
 dateList = []
 
 # ----------------------------------------------------------------------------------------------
 
+# creates the folder tracker tab for the GUI
 def create_tab(app):
 
-    #this function is for creating all the stuff we see on the Folder Tracker tab
-
+    # box entity that sits inside something in the window to contain stuff
     vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=BORDER_WIDTH)
     
-    #"Locate Text File" button
+    # "Locate Text File" button
     locate_button = Gtk.Button(label="Locate Text File")
     locate_button.connect("clicked", lambda widget: on_locate_file_clicked(app))
     vbox.pack_start(locate_button, False, False, 0)
     
-    #"Replay" button
+    # "Replay" button
     replay_button = Gtk.Button(label="Replay")
     replay_button.set_sensitive(False)
     replay_button.connect("clicked", lambda widget: on_replay_clicked(app))
     vbox.pack_start(replay_button, False, False, 0)
     
-    #listbox for the replay options
+    # listbox for the replay options
     journal_entries_box = Gtk.ListBox()
     journal_entries_box.set_selection_mode(Gtk.SelectionMode.SINGLE)
     journal_entries_box.connect("row-selected", lambda box, row: on_row_selected(app, row))
     vbox.pack_start(journal_entries_box, True, True, 0)
 
-    #assign the listbox and buttons to the app
+    # assign the listbox and buttons to the app
     app.journal_entries_box = journal_entries_box
     app.locate_button = locate_button
     app.replay_button = replay_button
     
     return vbox
 
+# enables the replay button when a journal entry is selected.
+# by default, the replay button is greyed out.
 def on_row_selected(app, row):
     app.replay_button.set_sensitive(row is not None)
 
 # ----------------------------------------------------------------------------------------------
 
-#when the locate file button is clicked
+# when the locate file button is clicked
 def on_locate_file_clicked(app):
 
     global journal
 
-    #brings up the file explorer when the locate file button is pressed
+    # brings up the file explorer when the locate file button is pressed
     dialog = Gtk.FileChooserDialog(
         title="Select a text (.txt) file.",
         parent=app,
@@ -87,33 +91,34 @@ def on_locate_file_clicked(app):
     )
     dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
 
-    #make it so user can only see .txt files to select
+    # filter to only display .txt files in the file explorer
     file_filter = Gtk.FileFilter()
     file_filter.set_name("Text Files")
     file_filter.add_pattern("*.txt")
     dialog.add_filter(file_filter)
     
-    #takes the response from the file explorer and
-    #gets the file path to put in to the journal gathering functions
+    # takes the response from the file explorer and
+    # gets the file path to put in to the journal gathering functions
     response = dialog.run()
     if response == Gtk.ResponseType.OK:
+        
         pathStr = dialog.get_filename()
         fileID = os.stat(pathStr).st_ino
         pythonFileLocation = pathStr.rsplit("/", 1)[0]
         pythonFileLocation = pythonFileLocation+ "/.NATracker/" + str(fileID) + ".journal"
         print(pythonFileLocation)
         
-        #gets the location of the journal and uses that to store
-        #the journal object created from WatchThisFolder.py in the
-        #global journal variable to be accessed from anywhere in replay.py
+        # gets the location of the journal and uses that to store
+        # the journal object created from WatchThisFolder.py in the
+        # global journal variable to be accessed from anywhere in replay.py
         journal = getJournal(pythonFileLocation)
         
-        #clearing the lists incase they have stuff in them already
+        # clearing existing data in the lists
         dateList.clear()
         stringList.clear()
         
-        #adding items from the journal to dateList and stringList.
-        #the indexes of dateList and stringList correlate
+        # adding items from the journal to dateList and stringList.
+        # the indexes of dateList and stringList correlate
         for i in journal.JournalEntrys:
             dateList.append(datetime.datetime.fromtimestamp(i[0][3]
 ).strftime('%Y-%m-%d %H:%M:%S'))
@@ -121,7 +126,7 @@ def on_locate_file_clicked(app):
         #print (dateList)
         #print (stringList)
         
-        #populating the listBox with the date and times from dateList
+        # populating the listBox with the date and times from dateList
         for date in dateList:
             row = Gtk.ListBoxRow()
             label = Gtk.Label(label=date, xalign=0)
@@ -129,13 +134,13 @@ def on_locate_file_clicked(app):
             app.journal_entries_box.add(row)
         app.journal_entries_box.show_all()
         
-    #if no journal is found for a text file 
+    # if no journal is found for a text file 
     else:
         show_error_message(app, "No journal found", "There was no journal found for this text.")
     dialog.destroy()  
 
 # ----------------------------------------------------------------------------------------------
-
+ # loads a journal object from a file
 def getJournal(fileName):
     #try:
     #    fileID = os.stat(currentDir+"/" +fileName).st_ino
@@ -148,7 +153,7 @@ def getJournal(fileName):
     with open(fileName, "rb") as journalFile:
         return listunhack(pickle.load(journalFile))
 
-#pickle problems
+# functions to solve pickle problems
 def listhack(journal):
     return journal.JournalID, journal.JournalLimit, journal.JournalEntrys, journal.contentsBeforeDiff, journal.maxLineCount, journal.seed
 
@@ -163,6 +168,8 @@ def listunhack(list):
 
 # ----------------------------------------------------------------------------------------------
 
+# function to translate the journal list entries into strings
+# to put into the string list that is displayed in the listbox
 def dictToString(dict): 
     string = ""
     for line in dict:
@@ -172,9 +179,12 @@ def dictToString(dict):
 
 # ----------------------------------------------------------------------------------------------
 
+# when the replay button is clicked
 def on_replay_clicked(app):
+    
     # Get the selected row's index
     selected_row = app.journal_entries_box.get_selected_row()
+    
     if selected_row:
         index = app.journal_entries_box.get_children().index(selected_row)
     
@@ -184,7 +194,10 @@ def on_replay_clicked(app):
         
         open_replay_window(app, entry_to_replay)
 
+# called when replay button is pressed to allow user to traverse
+# the file explorer to select a location the file will be saved in.
 def save_file_dialog(app, entry_to_replay):
+    
     # File chooser dialog to save the replayed file
     dialog = Gtk.FileChooserDialog(
         title="Save the reconstructed file",
@@ -200,6 +213,7 @@ def save_file_dialog(app, entry_to_replay):
         recreateUpToEntry(entry_to_replay, save_path)
     dialog.destroy()
 
+# opens a new window to display the content of the selected journal entry
 def open_replay_window(parent, entry_to_replay):
     # Create the replay window
     replay_window = Gtk.Window(title="Replay Entry")
@@ -237,6 +251,7 @@ def open_replay_window(parent, entry_to_replay):
     replay_window.add(vbox)
     replay_window.show_all()
 
+# called when replay button is pressed to create the file.
 def save_replayed_file(parent, replayed_content):
     # File chooser dialog to save the replayed file
     dialog = Gtk.FileChooserDialog(
@@ -255,6 +270,7 @@ def save_replayed_file(parent, replayed_content):
 
 # ----------------------------------------------------------------------------------------------
 
+# called to create the text file
 def recreateFile(diffJournal):
     file = {}
     #insert contents before diff
@@ -269,6 +285,7 @@ def recreateFile(diffJournal):
                 file[journalEntry[0]] = ""
     return dictToSting(file)
 
+# called to create the text file but only up to a specified change
 def recreateUpToEntry(diffJournal, entry):
     file = {}
     #insert contents before diff
@@ -290,6 +307,7 @@ def recreateUpToEntry(diffJournal, entry):
 
 # ----------------------------------------------------------------------------------------------
 
+# function to display an error message
 def show_error_message(app, title, message):
     dialog = Gtk.MessageDialog(
         transient_for=app,
