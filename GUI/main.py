bbaaf99688af5092a95a2a55e9d1b38e5477cf29
Zach Stofko <90708909+PresidentZach@users.noreply.github.com>
2024-12-08 01:30:34 -0500

    Update main.py

--- GUI/main.py
+++ GUI/main.py
@@ -1,16 +1,16 @@
-import gi #library that provides python to gnome translation
-gi.require_version("Gtk", "3.0")  #specify the version before importing Gtk or Gdk
-from gi.repository import Gtk, Gio #for the window
-from gi.repository import Gdk #for the screen / display
-import os #os library
+import gi # library that provides python to gnome translation
+gi.require_version("Gtk", "3.0")  # specify the version before importing Gtk or Gdk
+from gi.repository import Gtk, Gio # for the window
+from gi.repository import Gdk # for the screen / display
+import os
 
-#import modules for functions of the program
+# import modules for functions of the program
 import tracker
 import replay
 
 #----------------------------------------------------------------------------------------------
 
-#gui settings
+# gui settings
 BORDER_WIDTH = 12
 PROGRAM_NAME = "NATracker"
 TAB1_NAME = "Folder Tracker"
@@ -20,10 +20,10 @@ PROGRAM_MIN_HEIGHT = 480
 
 #----------------------------------------------------------------------------------------------
 
-#main gtk window class
+# main gtk window class
 class program_window(Gtk.Window):
 
-    #initialize the program_window object
+    # initialize the program_window object
     def __init__(self):
     
         super().__init__(title=PROGRAM_NAME)
@@ -50,28 +50,28 @@ class program_window(Gtk.Window):
         notebook = Gtk.Notebook()
         self.add(notebook)
 
-        #creating the tabs---------------------------------------------------------------------
+        # creating the tabs---------------------------------------------------------------------
 
-        #"Folder Tracker" tab
-        self.tracked_folders = set()  #initialize a set for the tracked folders
-        self.folder_list_box = None  #initialize a listbox for the folders
+        # "Folder Tracker" tab
+        self.tracked_folders = set()  # initialize a set for the tracked folders
+        self.folder_list_box = None  # initialize a listbox for the folders
         tab_folderTracker = tracker.create_tab(self)
         notebook.append_page(tab_folderTracker, Gtk.Label(label=TAB1_NAME))
 
-        #"Replay" tab
+        # "Replay" tab
         tab_replay = replay.create_tab(self)
         notebook.append_page(tab_replay, Gtk.Label(label=TAB2_NAME))
 
 #----------------------------------------------------------------------------------------------
 
-#main function
+# main function
 if __name__ == "__main__":
 
-    app = program_window() #create the window
-    app.connect("destroy", Gtk.main_quit) #quits out of program if close button is pressed
-    app.show_all() #makes everything in the window visible
+    app = program_window() # create the window
+    app.connect("destroy", Gtk.main_quit) # quits out of program if close button is pressed
+    app.show_all() # makes everything in the window visible
 
-    #if the program is not running under root, it quits
+    # if the program is not running under root, it quits
     if os.geteuid() != 0:
         Gtk.main_quit()
     else:
