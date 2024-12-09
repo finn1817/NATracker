81e13cf8d990d70ed5d7abedfbe2af316f66413f
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2024-12-09 13:43:57 -0500

    Update replay.py

--- GUI/replay.py
+++ GUI/replay.py
@@ -302,7 +302,7 @@ def recreateUpToEntry(diffJournal, entry):
         returnOnNext = False
         for journalEntry in change:
             if journalEntry[2]:
-                file[journalEntry[0]] = journalEntry[1]
+                file[journalEntry[0]] = journalEntry[1] + str(journalEntry[0])
             else:
                 file[journalEntry[0]] = ""
             if change == entry:
