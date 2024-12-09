eaa6adc64ef3a4c91adcde11eb2368c58df3f29a
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2024-12-09 14:13:23 -0500

    Revert "asdf"
    This reverts commit e55ff131aa57482ab4fc6e4face06735e197a43c.

--- GUI/replay.py
+++ GUI/replay.py
@@ -178,9 +178,11 @@ def listunhack(list):
 # to put into the string list that is displayed in the listbox
 def dictToString(dict): 
     #sort the dictionary by key
+    dict = dict.items()
+    dict = sorted(dict)
     string = ""
     for line in dict:
-        string += line + "\n"
+        string += dict[line] + "\n"
     return string
 
 
