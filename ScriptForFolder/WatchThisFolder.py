4c7813e63afaaceb1bf4b555ae3ef9f59aeb10cc
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2024-12-10 01:09:15 -0500

    Revert "sadasd"
    This reverts commit 5792be0e98937821a55dffbd95b016aa319a34da.

--- ScriptForFolder/WatchThisFolder.py
+++ ScriptForFolder/WatchThisFolder.py
@@ -83,8 +83,7 @@ def watcher():
                 #check if Inode matches iNodeDict
                 if os.path.exists(currentDir + "/.NATracker/" + str(inodeDict[event]) + ".journal"):
                     os.rename(currentDir + "/.NATracker/" + str(inodeDict[event]) + ".journal", currentDir + "/.NATracker/" + str(os.stat(currentDir + "/" + event).st_ino) + ".journal")
-                else:
-                    creation.append(event.name)
+                creation.append(event.name)
             elif event.mask == 64: #File Moved away
                 print ("File Moved Away: " + event.name)
                 deletion.append(event.name)
